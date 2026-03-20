43a67c6556f9e8707f665fc3cf24910b63a639f0
Rickard Bremer <rickard.bremer@gmail.com>
2026-03-20 11:22:58 +0100

    Map markers, show info and goto new activity

---
+++ app/src/main/java/com/example/enstorstark/Location.java
@@ -0,0 +1,13 @@
+package com.example.enstorstark;
+
+public class Location {
+    public String name;
+    public double lat;
+    public double lng;
+
+    public Location(String name, double lat, double lng) {
+        this.name = name;
+        this.lat = lat;
+        this.lng = lng;
+    }
+}
--- app/src/main/java/com/example/enstorstark/MapActivity.java
+++ app/src/main/java/com/example/enstorstark/MapActivity.java
@@ -1,5 +1,6 @@
 package com.example.enstorstark;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.preference.PreferenceManager;
@@ -13,6 +14,9 @@ import org.osmdroid.views.overlay.compass.CompassOverlay;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapActivity extends AppCompatActivity {
 
     private MapView map;
@@ -21,7 +25,6 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Must be done before setContentView
         Configuration.getInstance().load(getApplicationContext(),
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         Configuration.getInstance().setUserAgentValue(getPackageName());
@@ -32,11 +35,10 @@ public class MapActivity extends AppCompatActivity {
         map.setTileSource(TileSourceFactory.MAPNIK);
         map.setMultiTouchControls(true);
 
-        // Set start position and zoom
         map.getController().setZoom(15.0);
-        map.getController().setCenter(new GeoPoint(57.7089, 11.9746)); // Gothenburg
+        map.getController().setCenter(new GeoPoint(57.7089, 11.9746));
 
-        // Show my location
+        // My location
         MyLocationNewOverlay locationOverlay = new MyLocationNewOverlay(
                 new GpsMyLocationProvider(this), map);
         locationOverlay.enableMyLocation();
@@ -46,6 +48,42 @@ public class MapActivity extends AppCompatActivity {
         CompassOverlay compassOverlay = new CompassOverlay(this, map);
         compassOverlay.enableCompass();
         map.getOverlays().add(compassOverlay);
+
+        addMarkers();
+    }
+
+    private List<Location> getLocations() {
+        List<Location> locations = new ArrayList<>();
+        locations.add(new Location("Point A", 57.7089, 11.9746));
+        locations.add(new Location("Point B", 57.7100, 11.9800));
+        locations.add(new Location("Point C", 57.7050, 11.9700));
+        return locations;
+    }
+
+    private void addMarkers() {
+        for (Location loc : getLocations()) {
+            Marker marker = new Marker(map);
+            marker.setPosition(new GeoPoint(loc.lat, loc.lng));
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER);
+            marker.setTitle(loc.name);
+            marker.setSnippet("Lat: " + loc.lat + ", Lng: " + loc.lng);
+
+            marker.setOnMarkerClickListener((m, mapView) -> {
+                if (m.isInfoWindowShown()) {
+                    Intent intent = new Intent(MapActivity.this, PubDetailsActivity.class);
+                    intent.putExtra("name", loc.name);
+                    intent.putExtra("lat", loc.lat);
+                    intent.putExtra("lng", loc.lng);
+                    startActivity(intent);
+                } else {
+                    m.showInfoWindow();
+                }
+                return true;
+            });
+
+            map.getOverlays().add(marker);
+        }
+        map.invalidate();
     }
 
     @Override
